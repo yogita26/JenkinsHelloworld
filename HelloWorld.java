bf78c6c6d48b3fcbc5c9528e06df9987a7dd7079
yogita26 <47413658+yogita26@users.noreply.github.com>
2020-11-10 17:45:14 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-   System.out.println("******* made some changes******Hello this is our 1st jenkins job");
+   System.out.println("******* made some changes******Hello");
 	}
 
 }
